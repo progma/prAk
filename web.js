a7d8f9fa0f221bd8d83e0a7772cbd129ace8596b
Lukáš Lánský <lukas@lansky.name>
2012-06-29 19:46:19 +0200

    Tweaking web.js

--- web.js
+++ web.js
@@ -9,15 +9,12 @@ var file = new(static.Server)(webroot, {
 });
 http.createServer(function(req, res) {
   req.addListener('end', function() {
+    console.log(req);
     file.serve(req, res, function(err, result) {
       if (err) {
         console.error('Error serving %s - %s', req.url, err.message);
-        if (err.status === 404 || err.status === 500) {
-          file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
-        } else {
-          res.writeHead(err.status, err.headers);
-          res.end();
-        }
+        res.writeHead(err.status, err.headers);
+        res.end();
       } else {
         console.log('%s - %s', req.url, res.message);
       }
